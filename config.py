17cc4dfbe394b074427d1595d190a57d6e6ea7d0
Elieghan <78752030+Elieghan@users.noreply.github.com>
2021-03-10 17:51:23 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+
+# Config File
+
+
+# Lists of URLs
+
+DB_URL_MEETUP = 'https://www.meetup.com/find/?keywords=data%20science'
+DB_URL = 'https://www.meetup.com'
+FIND = '/find/?keywords='
+LIMIT = 30
+
+# DATABASE
+DATABASE_NAME = 'meetup'
+PASSWORD_DB_SERVER ='password'
+
+# Default types and location:
+
+DB_TYPE = 'data science'
+
