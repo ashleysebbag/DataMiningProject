44936f77fe62bec358e594e73b129762eebd7019
Elieghan <78752030+Elieghan@users.noreply.github.com>
2021-03-30 20:55:49 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -11,11 +11,13 @@ Desc    : Config file of Meetup Scrapper
 
 MEETUP_URL = 'https://www.meetup.com'
 DEFAULT_MEETUP_URL = 'https://www.meetup.com/find/?keywords=data%20science'
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+API_URL = 'https://restcountries.eu/rest/v2/name/'
 
 FIND = '/find/?keywords='
 
 # DATABASE
-DATABASE_NAME = 'meetup'
-PASSWORD_DB_SERVER = 'password'
 HOST = 'localhost' 
+DATABASE_NAME = 'meetup'
+PASSWORD_DB_SERVER = 'ibaiWo3n'
 
