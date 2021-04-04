f0ad222ecf1c95a4033ced819a2b076c21f26bc4
ashleysebbag <78728544+ashleysebbag@users.noreply.github.com>
2021-04-04 18:07:34 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+Name    : DB.py
+Author  : Ashley_SEBBAG Elie_GHANASSIA
+Contact : ashsebbag@gmail.com elieghan@yahoo.fr
+Time    : 05/03/2021 17:22
+Desc    : Config file of Meetup Scrapper
+"""
+
+
+MEETUP_URL = 'https://www.meetup.com'
+DEFAULT_MEETUP_URL = 'https://www.meetup.com/find/?keywords=data%20science'
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+API_URL = 'https://restcountries.eu/rest/v2/name/'
+
+FIND = '/find/?keywords='
+
+# DATABASE
+HOST = 'localhost'
+DATABASE_NAME = 'meetup'
+PASSWORD_DB_SERVER = 'password'
+
